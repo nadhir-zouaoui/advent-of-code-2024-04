3bca17d1010cb5f322a33e0468dd8922324e027f
nadhir zouaoui <nadhirzouaoui256@gmail.com>
2024-12-04 22:55:56 +0100

    still a logic issue there but working on it

--- advent-of-code-2024-04/advent-of-code-2024-04.cpp
+++ advent-of-code-2024-04/advent-of-code-2024-04.cpp
@@ -4,56 +4,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-int valide(std::string ch) {
-    if (ch == "XMAS")
+int valide(std::string ch, std::string ch1) {
+    if (ch == "MAS" && ch==ch1)
         return 1;
     else return 0;
-}
-std::string behindX(std::string content, int i) {
-    std::string test;
-    int j = i;
-    while (j >=0 && j > i - 4) {
-        test += content[j];
-        j--;
-    }
-    return test;
-}
-std::string inFrontOfX(std::string content, int maxLength, int i) {
-    std::string test;
-    int j = i;
-    while (j < maxLength && j < i + 4) {
-        test += content[j];
-        j++;
-    }
-    return test;
-}
-std::string underX(std::string content, int maxLength, int i, int lineSize) {
-    std::string test;
-    int j = i;
-    int count = 0;
-    while (j < maxLength && count < 4) {
-        test += content[j];
-        j+= lineSize;
-        count++;
-    }
-    return test;
-}
-std::string upperX(std::string content, int maxLength, int i, int lineSize) {
-    std::string test;
-    int j = i;
-    int count = 0;
-    while (j >= 0 && count < 4) {
-        test += content[j];
-        j -= lineSize;
-        count++;
-    }
-    return test;
 }
 std::string rightLowerDiag(std::string content, int maxLength, int i, int lineSize) {
     std::string test;
     int j = i;
     int count = 0;
-    while (j < maxLength && count < 4) {
+    while (j < maxLength && count < 3) {
         test += content[j];
         j += lineSize+1;
         count++;
@@ -64,7 +24,7 @@ std::string leftLowerDiag(std::string content, int maxLength, int i, int lineSiz
     std::string test;
     int j = i;
     int count = 0;
-    while (j < maxLength && count < 4) {
+    while (j < maxLength && count < 3) {
         test += content[j];
         j += lineSize - 1;
         count++;
@@ -75,7 +35,7 @@ std::string rightUpperDiag(std::string content, int maxLength, int i, int lineSi
     std::string test;
     int j = i;
     int count = 0;
-    while (j >= 0 && count < 4) {
+    while (j >= 0 && count < 3) {
         test += content[j];
         j = j - lineSize + 1;
         count++;
@@ -86,7 +46,7 @@ std::string leftUpperDiag(std::string content, int maxLength, int i, int lineSiz
     std::string test;
     int j = i;
     int count = 0;
-    while (j >= 0 && count < 4) {
+    while (j >= 0 && count < 3) {
         test += content[j];
         j -= lineSize +1 ;
         count++;
@@ -109,28 +69,19 @@ int main()
     std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     int s = 0;
     int maxLength = content.size();
-    int j;
+    std::string test1;
+    std::string test2;
     for (int i = 0; i < maxLength; i++)
     {
-        if (content[i]=='X')
+        if (content[i]=='M')
         {
-            std::string test;
-            test = behindX(content, i);
-            s += valide(test);
-            test = inFrontOfX(content, maxLength, i);
-            s += valide(test);
-            test = underX(content, maxLength, i, lineSize);
-            s += valide(test);
-            test = upperX(content, maxLength, i, lineSize);
-            s += valide(test);
-            test = rightLowerDiag(content, maxLength, i, lineSize);
-            s += valide(test);
-            test = leftLowerDiag(content, maxLength, i, lineSize);
-            s += valide(test);
-            test = rightUpperDiag(content, maxLength, i, lineSize);
-            s += valide(test);
-            test = leftUpperDiag(content, maxLength, i , lineSize);
-            s += valide(test);
+            test1 = rightLowerDiag(content, maxLength, i, lineSize);
+            i += 2;
+            test2 = leftLowerDiag(content, maxLength, i, lineSize);
+            s += valide(test1,test2);
+            test1 = rightUpperDiag(content, maxLength, i, lineSize);
+            test2 = leftUpperDiag(content, maxLength, i , lineSize);
+            s += valide(test1,test2);
         }
     }
     std::cout << s << std::endl;
